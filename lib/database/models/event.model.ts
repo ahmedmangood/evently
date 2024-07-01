4a0a79fcb315817a928b044d4618b181fe05f890
ahmedmangood <ahmedmangood656@gmail.com>
2024-07-01 13:22:09 +0300

    fix the issues

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -9,7 +9,7 @@ export interface IEvent extends Document {
   imageUrl: string;
   startDateTime: Date;
   endDateTime: Date;
-  price?: string;
+  price: string;
   isFree: boolean;
   url?: string;
   category: { _id: string; name: string };
@@ -17,11 +17,11 @@ export interface IEvent extends Document {
 }
 
 const EventSchema = new Schema({
-  title: { type: String, require: true },
+  title: { type: String, required: true },
   description: { type: String },
   location: { type: String },
   createdAt: { type: Date, default: Date.now },
-  imageUrl: { type: String, require: true },
+  imageUrl: { type: String, required: true },
   startDateTime: { type: Date, default: Date.now },
   endDateTime: { type: Date, default: Date.now },
   price: { type: String },
