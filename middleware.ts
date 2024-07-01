0ec53c6753e76944bcca601eb09af5170770b6f3
ahmedmangood <ahmedmangood656@gmail.com>
2024-07-01 11:32:04 +0300

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,9 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
-
-export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
-    auth().protect();
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
+  // The following matcher runs middleware on all routes
+  // except static assets.
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
