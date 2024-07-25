fd82473378371c82e1804caa40771d31c9738af8
ahmedmangood <ahmedmangood656@gmail.com>
2024-07-25 18:51:42 +0300

    new fixed

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -6,31 +6,21 @@ let cached = (global as any).mongoose || { con: null, promise: null };
 
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
-
+  console.log("Iam Here");
   if (!MONGODB_URL) throw new Error("MONGODB_URL is missing");
-
-  // cached.promise =
-  //   cached.promise ||
-  //   mongoose.connect(MONGODB_URL, {
-  //     dbName: "evently",
-  //     bufferCommands: false,
-  //   });
-
-  // try {
-  //   cached.conn = await cached.promise;
-  //   console.log("New MongoDB connection established");
-  //   return cached.conn;
-  // } catch (error) {
-  //   console.error("MongoDB connection error:", error);
-  //   throw error;
-  // }
 
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URL, {
       dbName: "evently",
-      bufferCommands: false,
     });
-  cached.conn = await cached.promis;
-  return cached.conn;
+
+  try {
+    cached.conn = await cached.promise;
+    console.log("New MongoDB connection established");
+    return cached.conn;
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    throw error;
+  }
 };
