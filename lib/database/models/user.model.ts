33c38710fa0499656abba3139486082f5f9827c5
ahmedmangood <ahmedmangood656@gmail.com>
2024-07-25 19:22:34 +0300

    update action file

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,11 +1,11 @@
 import { Schema, model, models } from "mongoose";
 
 const UserSchema = new Schema({
-  clarkId: { type: String, require: true, unique: true },
+  clerkId: { type: String, require: true, unique: true },
   email: { type: String, require: true, unique: true },
   username: { type: String, require: true, unique: true },
-  firstname: { type: String, requir: true, unique: true },
-  lastname: { type: String, require: true, unique: true },
+  firstName: { type: String, requir: true },
+  lastName: { type: String, require: true },
   photo: { type: String, require: true },
 });
 
--- types/index.ts
+++ types/index.ts
@@ -1,11 +1,11 @@
 // ====== USER PARAMS
 export type CreateUserParams = {
-  clerkId: string;
-  firstName: string;
-  lastName: string;
-  username: string;
-  email: string;
-  photo: string;
+  clerkId: String;
+  firstName: String;
+  lastName: String;
+  username: String;
+  email: String;
+  photo: String;
 };
 
 export type UpdateUserParams = {
